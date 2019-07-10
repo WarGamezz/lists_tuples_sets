90b19634da0a67b863a93cd52eb47894900f14a4
WarGamezz <widofmaker@gmail.com>
2019-07-10 01:10:51 -0400

    Commit

--- lists_tuples_sets_dictionaries.py
+++ lists_tuples_sets_dictionaries.py
@@ -2,4 +2,27 @@
 # Created using multiple sources including: tutorialspoint.com; python-course.eu; w3schools.com; Corey Schafer's YouTube tutorial. I do not mean to infringe any copyright laws, this file was compiled to put all of the knowledge required in one place. If there's any issue with the copyrights, please message me and I will delete requested data immediately.
 # Using Visual Studio Code on Windows machine, using better comments extension for better visuals
 # Word wrap is set.
-# Git https://github.com/WarGamezz/lists_tuples_sets
+# Git https://github.com/WarGamezz/lists_tuples_sets_dictionaries
+
+#! Sequential Data Types
+# Python provides for six sequence (or sequential) data types:
+# strings
+# byte sequences
+# byte arrays
+# lists
+# tuples
+# range objects
+
+# Strings, lists, tuples, bytes and range objects may look like utterly different things, but nevertheless they have some underlying concepts in common:
+# * The items or elements of strings, lists and tuples are ordered in a defined sequence
+# * The elements can be accessed via indices
+
+#? strings
+# refer to hello_world GIT
+
+#? byte sequences - don't see it being usefull at the moment
+# The bytes() method returns a bytes object which is an immmutable (cannot be modified) sequence of integers in the range 0 <=x < 256.
+
+#? byte arrays  - don't see it being usefull at the moment
+#bytearray() objects are a mutable counterpart to bytes objects.
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+text = 'This is my dog'
+
+def reversal (text):
+    lista = list(text)
+for x in range(0:(len(text)-1):
+    print((list[len]-x)
+    return(text)
